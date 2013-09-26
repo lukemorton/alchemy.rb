15333aa9eb9481d55a7d86c2d300b708335ec721
Luke Morton <lukemorton.dev@gmail.com>
2013-09-26 17:14:56 +0100

    Fix tests (doh!)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,40 @@ require 'coveralls'
 Coveralls.wear!
 
 require_relative '../lib/app/data/mapper/users'
+
+require_relative '../lib/app/data/mapper/users'
+require_relative '../lib/app/data/action/login'
+require_relative '../lib/app/data/model/welcome'
+
 require_relative '../lib/app/view/template_engine/hash'
+require_relative '../lib/app/view/model/login'
+require_relative '../lib/app/view/model/welcome'
 
 def request(request = {})
-  request[:mappers] = {
-    :users => App::Data::Mapper::Users.new,
+  request[:data] = {
+    :mappers => {
+      :users => App::Data::Mapper::Users.new,
+    },
+
+    :model => {
+      :welcome => App::Data::Model::Welcome.new,
+    },
+
+    :action => {
+      :login => App::Data::Action::Login.new,
+    }
   }
 
-  request[:view] = App::View::TemplateEngine::Hash.new
+  request[:view] = {
+    :template_engine => {
+      :mustache => App::View::TemplateEngine::Hash.new,
+    },
+
+    :model => {
+      :login => App::View::Model::Login.new,
+      :welcome => App::View::Model::Welcome.new,
+    },
+  }
   
   request[:session] ||= {}
   request[:post] ||= {}
